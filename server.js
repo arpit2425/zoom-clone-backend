855518e48ff30f0d41b3e0102d7b8ee00735716b
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-10-05 11:54:44 +0530

    Add view enginr

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('room');
 });
 app.listen(3030, (req, res) => {
     console.log('Listening ');
