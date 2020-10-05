e18949c24c5a16021b55070b015f2feefc8a6779
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-10-05 12:27:15 +0530

    Add video element

---
+++ public/script.js
@@ -0,0 +1,19 @@
+let myVideoStream;
+const myVideo=document.createElement('video');
+const videoGrid=document.getElementById('video-grid');
+myVideo.muted=true;
+navigator.mediaDevices.getUserMedia({
+video:true,
+audio:true,
+}).then(stream=>{
+myVideoStream=stream;
+addVideoStream(myVideo,stream)
+});
+const addVideoStream=(video,stream)=>{
+video.srcObject=stream;
+video.addEventListener('loadedmetadata',()=>{
+video.play();
+})
+videoGrid.append(video);
+
+}
